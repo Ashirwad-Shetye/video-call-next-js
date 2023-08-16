765f167c32dfc1263841a5c41c8e3ded4f88e2bf
Ashirwad P Shetye <99337222+Ashirwad-Shetye@users.noreply.github.com>
2023-08-17 04:45:48 +0530

    chore:helper funcs

---
+++ src/helpers/index.ts
@@ -0,0 +1,9 @@
+import createMeetingCode from "./create-meeting-code";
+import getCurrentDay from "./generate-day";
+import getCurrentTime from "./generate-date";
+
+export {
+    createMeetingCode,
+    getCurrentTime,
+    getCurrentDay
+}
